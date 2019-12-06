708d623c422ac63f213c92f0db36f736edc64586
haamdii <haddad.officiel@gmail.com>
2019-12-06 01:00:30 +0100

    Second commit

--- app/src/main/java/com/example/dhiyabh/jean_cloudhelper/ConnexionActicity.kt
+++ app/src/main/java/com/example/dhiyabh/jean_cloudhelper/ConnexionActicity.kt
@@ -14,6 +14,8 @@ class ConnexionActicity : AppCompatActivity() {
     private lateinit var auth: FirebaseAuth
 
 
+
+    
         override fun onCreate(savedInstanceState: Bundle?) {
             super.onCreate(savedInstanceState)
             setContentView(R.layout.activity_login)
